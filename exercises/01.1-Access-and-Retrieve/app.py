ade4c598b871e6144e2599b1cd88992af9d8b579
gabsrodriguez <gmederos95@gmail.com>
2020-01-12 18:32:36 +0000

    first commit

--- exercises/01.1-Access-and-Retrieve/app.py
+++ exercises/01.1-Access-and-Retrieve/app.py
@@ -2,7 +2,14 @@
 my_list = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
 
 #1. print the item here
+print(my_list[2])
 
 #2. change 'thursday'a value here to None
+my_list.remove("thursday")
+
+my_list.insert(4,"None")
+print(my_list)
+
 
 #3. print the position of step 2
+print(my_list[4])
